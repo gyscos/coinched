8077b54aae9e3593fddb24d5d46f4b526760e6c0
Alexandre Bury <alexandre.bury@gmail.com>
2015-12-26 13:30:03 -0800

    Move libcoinche out of the repository
    Still there at https://github.com/Gyscos/libcoinche

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate iron;
 extern crate bodyparser;
 extern crate rustc_serialize;
 extern crate eventual;
+extern crate libcoinche;
 
-pub mod libcoinche;
 pub mod game_manager;
 pub mod http;
